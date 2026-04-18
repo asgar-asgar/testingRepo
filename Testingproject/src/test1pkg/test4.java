c315e12144b2d795f4081848e01be945cf6a9cf8
Asgar Ali <asgarali2813127@gamil.com>
2026-04-19 01:45:49 +0530

    5 th commit

---
+++ Testingproject/src/test1pkg/test4.java
@@ -0,0 +1,7 @@
+package test1pkg;
+
+public class test4 {
+public static void main(String[] args) {
+	System.out.println("this is thired main class");
+}
+}
---
+++ Testingproject/src/test1pkg/test5.java
@@ -0,0 +1,9 @@
+package test1pkg;
+
+
+
+public class test5 {
+	public static void main(String[] args) {
+		System.out.println("hellp");
+	}
+}
