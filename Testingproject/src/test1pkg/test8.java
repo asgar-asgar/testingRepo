ff7d97f706151073dbdbe76f7669fa97c2cafc3d
Asgar Ali <asgarali2813127@gamil.com>
2026-04-19 02:35:41 +0530

    fixing the repo

---
+++ Testingproject/src/test1pkg/test8.java
@@ -0,0 +1,8 @@
+package test1pkg;
+
+public class test8 {
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+	}
+}
---
+++ Testingproject/src/test1pkg/testt.java
@@ -0,0 +1,8 @@
+package test1pkg;
+\
+
+public class testt {
+	public static void main(String[] args) {
+		System.out.println("ggggg");
+	}
+}
